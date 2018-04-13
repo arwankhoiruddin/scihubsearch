bf181ea83844553a690feb61e2cffd822b7c2dcc
arwan khoiruddin <arwan.khoiruddin@gmail.com>
2018-04-13 11:41:59 +0800

    updated to the link automatically forwarding to scihub

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
+var scihubURL = "https://whereisscihub.now.sh/go/";
+
 searchSciHub = function(word, tab){
   var query = word.selectionText;
   if (query == undefined) {
     var tabURL = tab.url;
-    chrome.tabs.update({url: "http://sci-hub.tw/" + tabURL});
+    chrome.tabs.update({url: scihubURL + tabURL});
   } else {
-    chrome.tabs.update({url: "http://sci-hub.tw/" + query});
+    chrome.tabs.update({url: scihubURL + query});
   }
 };
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   var tabURL = tab.url;
-  chrome.tabs.update({url: "http://sci-hub.tw/" + tabURL});
+  chrome.tabs.update({url: scihubURL + tabURL});
 });
 
 chrome.contextMenus.create({
